dc613c42aed46137de9c5daaf76709ae4a257429
Sankalp <45618047+sankalp1999@users.noreply.github.com>
2020-07-19 23:43:48 +0530

    Update and rename output.cpp to PointerRelated2.cpp

--- Lab8_9_related/PointerRelated2.cpp
+++ Lab8_9_related/PointerRelated2.cpp
@@ -1,9 +1,10 @@
+
 #include <iostream>
 using namespace std;
 int main (){
 		char c='z';
 		char *pc=&c;
-// sizeof returns the # of bytes…
+// sizeof returns the # of bytesâ€¦
     cout<<"\n pc= "<<pc<< " sizeof(pc): "<<sizeof(pc);
     cout<<"\n *pc= "<<*pc<<" sizeof(*pc): "<<sizeof(*pc);
     cout<<"\n &pc= "<<&pc<<" sizeof(&pc): "<<sizeof(&pc);
